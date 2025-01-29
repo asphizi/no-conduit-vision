b7ff0714d6d2311966add67e39b6e1802b810727
asphizi <asphizi@proton.me>
2025-01-29 10:18:41 +0100

    init repository

---
+++ src/client/java/io/github/asphizi/no_conduit_vision/NoConduitVisionClient.java
@@ -0,0 +1,10 @@
+package io.github.asphizi.no_conduit_vision;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoConduitVisionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
